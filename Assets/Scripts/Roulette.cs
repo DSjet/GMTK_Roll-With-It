5986654c98edc958a2dbff30c8abff3f3677aa57
feliseureka <feliseureka@gmail.com>
2022-07-17 12:05:53 +0700

    Fixing Roulette Script, Adding Dice Sprites

--- Assets/Scripts/Roulette.cs
+++ Assets/Scripts/Roulette.cs
@@ -2,70 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Roulette : MonoBehaviour
 {
 
     //References
-    public GameObject DiceSix;
-    public GameObject DiceTwenty;
+    public Image DiceSix;
+    public Sprite[] DiceSixSprites;
+    public TMP_Text DiceTwenty;
     public ValueRnd TotalEnemy;
-    private Vector2 InitialDiceSixValue;
-    private Vector2 InitialDiceTwentyValue;
-    private Rigidbody2D D6Rb;
-    private Rigidbody2D D20Rb;
-    public float force = 10f;
-    public float friction = 1f;
-    private float currForce;
-    
-
-    public void Start(){
-        InitialDiceSixValue.Set(-6.094859f, -0.768065f);
-        InitialDiceTwentyValue.Set(-5.65f, 0.76f);
-        D6Rb = DiceSix.GetComponent<Rigidbody2D>();
-        D20Rb = DiceTwenty.GetComponent<Rigidbody2D>();
-    }
+    public int RandomizerDice = 15;
+    public float RandomizerWaitTime = 0.05f;
+    private int resDice6;
+    private int resDice20;
 
     // Roll Dice Function
     public void RollDiceSix(){
         int RandomizedValue = (TotalEnemy.LastMap + Random.Range(1,5)) % 6;
+        resDice6 = RandomizedValue;
         TotalEnemy.LastMap = RandomizedValue;
-        animationShowDice6(RandomizedValue);
     }
 
     public void RollDiceTwenty(){
         int RandomizedValue = Random.Range(1,20);
+        resDice20 = RandomizedValue;
         TotalEnemy.TotalEnemy += RandomizedValue;
-        animationShowDice20(RandomizedValue);
+        StartCoroutine("animateAll");
     }
 
-    private void animationShowDice6(int value){
-        DiceSix.transform.position = InitialDiceSixValue;
-        DiceSix.SetActive(true);
+    private IEnumerator animationShowDice6(){
+        for(int i = 0; i < RandomizerDice; i++){
+            DiceSix.sprite = DiceSixSprites[Random.Range(0,5)];
+            yield return new WaitForSeconds(RandomizerWaitTime);
+        }
+        DiceSix.sprite = DiceSixSprites[resDice6];
     }
 
-    private void animationShowDice20(int value){
-        DiceTwenty.transform.position = InitialDiceTwentyValue;
-        DiceTwenty.SetActive(true);
+    private IEnumerator animateAll(){
+        for(int i = 0; i < RandomizerDice; i++){
+            DiceSix.sprite = DiceSixSprites[Random.Range(0,5)];
+            DiceTwenty.SetText(Random.Range(1,20).ToString());
+            yield return new WaitForSeconds(RandomizerWaitTime);
+        }
+        DiceSix.sprite = DiceSixSprites[resDice6];
+        DiceTwenty.SetText(resDice20.ToString());
     }
+
+    private IEnumerator animationShowDice20(){
+        for(int i = 0; i < RandomizerDice; i++){
+            DiceTwenty.SetText(Random.Range(1,20).ToString());
+            yield return new WaitForSeconds(RandomizerWaitTime);
+        }
+        DiceTwenty.SetText(resDice20.ToString());
+    }
+
 
     public void startRoulette(){
-        currForce = force;
         RollDiceSix();
         RollDiceTwenty();
-    }
-
-    public void stopRoulette(){
-        DiceSix.SetActive(false);
-        DiceTwenty.SetActive(false);
-    }
-
-    public void FixedUpdate(){
-        if(currForce > 0){
-            currForce -= friction;
-            D6Rb.AddForce(new Vector2(currForce * Time.deltaTime, 0));
-            D20Rb.AddForce(new Vector2(currForce * Time.deltaTime, 0));
-        }
     }
 
     public void resetSOValue(){
