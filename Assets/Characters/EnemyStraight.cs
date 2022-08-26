b3b68e640692aab9976e14f22fa23f3b8a8a6087
NoodleReza <reza.brilliansah77@gmail.com>
2022-08-26 10:45:42 +0700

    Enemy flip fix

--- Assets/Characters/EnemyStraight.cs
+++ Assets/Characters/EnemyStraight.cs
@@ -4,29 +4,24 @@ using UnityEngine;
 
 public class EnemyStraight : MonoBehaviour
 {
-    Transform parent;
     Transform player;
     SpriteRenderer sprite;
 
     private void Start()
     {
-        parent = this.transform.parent;
         player = GameObject.FindGameObjectWithTag("Player").transform;
         sprite = this.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        this.transform.rotation = Quaternion.AngleAxis(-1 * transform.rotation.z, Vector3.forward);
-        var zRot = transform.rotation.eulerAngles.z * Mathf.Deg2Rad;
-
         if (player.position.x > this.transform.position.x)
         {
-            sprite.flipX = true;
+            sprite.flipY = false;
         }
         else
         {
-            sprite.flipX = false;
+            sprite.flipY = true;
         }
     }
 }
